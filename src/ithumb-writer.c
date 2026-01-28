06a0b68e1944fa367b707d8f8caab2c904ea65e6
TheFauxDreamer <11380631+TheFauxDreamer@users.noreply.github.com>
2026-01-28 23:23:41 +0800

    Update ithumb-writer.c

--- src/ithumb-writer.c
+++ src/ithumb-writer.c
@@ -728,7 +728,7 @@ ithumb_writer_handle_rotation (GdkPixbuf *pixbuf, guint *rotation)
   {
       return gdk_pixbuf_rotate_simple (pixbuf, *rotation);
   }
-  return g_object_ref (G_OBJECT (pixbuf));
+  return g_object_ref (pixbuf);
 }
 
 /* On the iPhone, thumbnails are presented as squares in a grid.
@@ -968,7 +968,7 @@ ithumb_writer_write_thumbnail (iThumbWriter *writer,
     else if (thumb->data_type == ITDB_THUMB_TYPE_PIXBUF)
     {
         Itdb_Thumb_Pixbuf *thumb_pixbuf = (Itdb_Thumb_Pixbuf *)thumb;
-        pixbuf = g_object_ref (G_OBJECT (thumb_pixbuf->pixbuf));
+        pixbuf = g_object_ref (thumb_pixbuf->pixbuf);
     }
 
     if (pixbuf == NULL)
